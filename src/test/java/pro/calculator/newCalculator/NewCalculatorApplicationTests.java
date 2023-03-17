a67575e9f6502f04399510a85c0eaa216f7267cd
evgeny_sem163 <evg82tol@gmail.com>
2023-03-18 00:54:52 +0300

    Spring2

---
+++ src/test/java/pro/calculator/newCalculator/NewCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.calculator.newCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
